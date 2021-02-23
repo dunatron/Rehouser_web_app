f36d0b302328ade3ef8ca5dbb1de537ce885787a
Dunatron <heath.dunlop.hd@gmail.com>
2021-02-23 21:15:36 +1300

    ff

--- pages/index.js
+++ pages/index.js
@@ -1,120 +1,10 @@
 import React, { Component } from 'react';
-import dynamic from 'next/dynamic';
-// import PropTypes from 'prop-types';
-import ParticleBanner from '@/Components/Banner/ParticleBanner';
-// import Banner from '@/Components/Banner/index';
-
-import { Button, Typography, IconButton } from '@material-ui/core';
-import ChangeRouteButton from '@/Components/Routes/ChangeRouteButton';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-
-import Dashboard from '@/Components/Dashboard';
-
-import HOME_PAGE_DASHBOARD_CONFIG from '@/Lib/configs/dashboards/homepageDashConf';
-
-// server side props
-// import { initializeApollo, addApolloState } from '@/Lib/apolloClient';
-// import { CURRENT_USER_QUERY } from '@/Gql/queries';
-
-// import { parseCookies, setCookie, destroyCookie } from 'nookies';
 
 import FileUploader from '@/Components/FileUploader';
-
-// const DynamicParticleBanner = dynamic(
-//   () => import('@/Components/Banner/ParticleBanner'),
-//   { ssr: false }
-// );
-
-const HomePageBannerBody = () => {
-  return (
-    <>
-      <Typography
-        variant="h1"
-        style={{ marginBottom: '64px', textAlign: 'center' }}>
-        Rehouser Ltd
-      </Typography>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          height: '112px',
-        }}>
-        <ChangeRouteButton
-          title="Free Appraisal"
-          variant="contained"
-          color="secondary"
-          route="/landlord/appraisals/add"
-          btnProps={{
-            size: 'large',
-            style: {
-              pointerEvents: 'all',
-            },
-          }}
-        />
-        <ChangeRouteButton
-          title="Search for rental"
-          variant="contained"
-          // color="primary"
-          route="/property-search"
-          btnProps={{
-            size: 'large',
-            style: {
-              pointerEvents: 'all',
-            },
-          }}
-        />
-      </div>
-      <Typography
-        variant="h4"
-        color="secondary"
-        style={{ marginTop: '32px', textAlign: 'center' }}>
-        Read More
-      </Typography>
-      <IconButton
-        style={{
-          marginTop: '16px',
-          pointerEvents: 'all',
-        }}
-        aria-label="read-more"
-        href="#banner-footer"
-        color="secondary">
-        <ArrowDownwardIcon
-          fontSize="large"
-          style={{
-            height: '3rem',
-            width: '3rem',
-          }}
-        />
-      </IconButton>
-    </>
-  );
-};
 
 const HomePage = props => {
   return (
     <div>
-      <ParticleBanner
-        imageSrc="images/banners/home-page-banner.jpg"
-        disablePointerEvents={true}
-        footer={[
-          <Typography
-            key={1}
-            variant="h5"
-            color="secondary"
-            style={{ textAlign: 'center', padding: '0 30px' }}>
-            Turning empty houses into friendly abodes
-          </Typography>,
-        ]}>
-        <HomePageBannerBody />
-      </ParticleBanner>
-      <Dashboard
-        config={HOME_PAGE_DASHBOARD_CONFIG}
-        elevation={0}
-        heading=""
-        intro=""
-      />
-
       <FileUploader
         title="Upload test file"
         files={[]}
@@ -123,15 +13,6 @@ const HomePage = props => {
         }}
         recieveFile={() => alert('FIle was recieved')}
       />
-      {/* fileParams: PropTypes.shape({
-    public_id: PropTypes.string,
-    filename: PropTypes.string,
-    folder: PropTypes.string,
-    resource_type: PropTypes.oneOf(['image', 'raw', 'video', 'auto']),
-    tags: PropTypes.arrayOf(PropTypes.string),
-    type: PropTypes.oneOf(['upload', 'private', 'authenticated']),
-    access_mode: PropTypes.oneOf(['public', 'authenticated']),
-  }).isRequired, */}
     </div>
   );
 };
