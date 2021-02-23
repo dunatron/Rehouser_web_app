5869c4f40e1970a9b70861e3420e089af112380a
Dunatron <heath.dunlop.hd@gmail.com>
2021-02-23 21:48:24 +1300

    ff

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import { ApolloProvider } from '@apollo/client';
 
 import withData from '../lib/withData';
 import Head from 'next/head';
-import { RecoilRoot } from 'recoil';
 import '../public/css/customToast.css';
 import '../public/css/geosuggest.css';
 import '../public/css/nprogress.css';
@@ -12,20 +11,18 @@ function RehouserApp(props) {
   // const apolloClient = useApollo(pageProps);
 
   return (
-    <RecoilRoot>
-      <ApolloProvider client={apollo}>
-        <Head>
-          {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-          />
-          {/* <script src="https://js.stripe.com/v3/" /> */}
-          <script src="https://cdn.rawgit.com/progers/pathseg/master/pathseg.js"></script>
-        </Head>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    </RecoilRoot>
+    <ApolloProvider client={apollo}>
+      <Head>
+        {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+        {/* <script src="https://js.stripe.com/v3/" /> */}
+        <script src="https://cdn.rawgit.com/progers/pathseg/master/pathseg.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
   );
 }
 
